e9d43fd89e7a8f33dd756a761181a252d35b3f7d
SLdLok <94218696+SLdLok@users.noreply.github.com>
2022-02-12 22:31:38 +0800

    Add files via upload

---
+++ web-platform-xee2ay/script.js
@@ -0,0 +1,36 @@
+console.log('Started');
+// Required for side-effects
+
+var db = firebase.firestore();
+
+function SignIn() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    var credential = result.credential;
+    var token = credential.accessToken;
+    var user = result.user;
+    console.log("LogIn already")
+  }).catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+    console.log("ERROR: " + error)
+  });
+}
+
+function SignOut() {
+  firebase.auth().signOut().then(() => {
+    console.log("Logout already")
+  }).catch((error) => {
+    console.log("ERROR: " + error)
+  });
+}
+
+db.collection("users").get().then(querySnapshot => {
+  querySnapshot.forEach(doc => {
+    console.log(doc.id, doc.data());
+  });
+});
